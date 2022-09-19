23edf220bcacdbeb263b75b31eaabdcb9d6be7dc
Gergely Kamaras <gergely.kamaras@gmail.com>
2022-09-19 15:00:36 +0200

    Add: item base class

---
+++ Assets/Source/Actors/Items/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonCrawl.Actors;
+
+namespace Assets.Source.Actors.Items
+{
+    public abstract class Item : Actor
+    {
+        public abstract void Pickup(Actor player);
+
+    }
+}
