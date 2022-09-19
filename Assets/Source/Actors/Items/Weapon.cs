3d85f7872cb9fb923ad08f5bb98064335d8bcd63
Gergely Kamaras <gergely.kamaras@gmail.com>
2022-09-19 15:01:39 +0200

    Add: weapon

---
+++ Assets/Source/Actors/Items/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonCrawl.Actors;
+
+namespace Assets.Source.Actors.Items
+{
+    internal class Weapon : Item
+    {
+        public override string DefaultName => "Weapon";
+        public override int DefaultSpriteId => 128;
+
+        public override void Pickup(Actor player)
+        {
+            // Apply change
+            // player.damage += 10;
+            // player._inventory.Add(this);
+        }
+    }
+}
