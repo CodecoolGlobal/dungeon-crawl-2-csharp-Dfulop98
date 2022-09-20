207dec5a3d9130e1cb4b9961a3a9cead8b701c9f
Dfulop98 <dobo.fulop@icloud.com>
2022-09-20 16:29:14 +0200

    Fix: change back actor awake func, create switch case SetSprite for get different sprites

--- Assets/Source/Actors/Static/StaticUtil.cs
+++ Assets/Source/Actors/Static/StaticUtil.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assets.Source.Actors.Static
 {
-    internal class StaticUtil
+    public class StaticUtil
     {
+        public Random Random = new Random();
+
+        public int RandomSprite(params int[] args)
+        {
+            //random sprite from the given pack
+
+            int number = Random.Next(0, 100);
+            if (number > 0 && number < 25)
+            {
+                return args[0];
+            }
+            else if (number >= 25 && number < 50)
+            {
+                return args[1];
+            }
+            else if (number >= 75 && number < 100)
+            {
+                return args[2];
+            }
+            return args[3];
+        }
     }
 }
