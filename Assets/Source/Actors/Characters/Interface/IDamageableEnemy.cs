d0f4103b1ed919c6b2349b9285058541c79663c4
Gergely Kamaras <gergely.kamaras@gmail.com>
2022-09-20 14:41:59 +0200

    Add: Interface for character damage handling and death

---
+++ Assets/Source/Actors/Characters/Interface/IDamageableEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonCrawl.Actors.Characters;
+
+namespace Assets.Source.Actors.Characters.Enemy
+{
+    internal interface IDamageableEnemy : IDamageable
+    {
+        public void OnDeath(Player player);
+        public void ApplyDamage(Player player);
+    }
+}
---
+++ Assets/Source/Actors/Characters/Interface/IDamageablePlayer.cs
@@ -0,0 +1,16 @@
+﻿using DungeonCrawl.Actors.Characters;
+using DungeonCrawl.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Source.Actors.Characters
+{
+    internal interface IDamageablePlayer : IDamageable
+    {
+        public void OnDeath();
+        public void ApplyDamage(Enemy.Enemy enemy);
+    }
+}
