53dd5f3796fd4bd79fe9c7b9881f63d8efd47ce7
Gergely Kamaras <gergely.kamaras@gmail.com>
2022-09-21 10:00:56 +0200

    Add: Event log class

---
+++ Assets/Source/Core/EventLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Source.Core
+{
+    public class EventLog
+    {
+        public List<string> Events = new List<string>();
+        public void AddEvent(string text)
+        {
+            Console.WriteLine("event");
+        }
+    }
+}
