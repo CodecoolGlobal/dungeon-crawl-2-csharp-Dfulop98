c7446babebe820d0ec2a593287c0f9e5d08b0cb6
Gergely Kamaras <gergely.kamaras@gmail.com>
2022-09-21 13:04:23 +0200

    Add: meaningful values to enemies

--- Assets/Source/Actors/Characters/Enemy/Spider.cs
+++ Assets/Source/Actors/Characters/Enemy/Spider.cs
@@ -4,8 +4,8 @@ namespace Assets.Source.Actors.Characters.Enemy
 {
     public class Spider : Enemy
     {
-        public override int Damage { get; set; } = 2;
-        public override int Health { get; set; } = 10;
+        public override int Damage { get; set; } = 10;
+        public override int Health { get; set; } = 30;
         public override int ScoreValue { get; set; } = 10;
 
         public override int DefaultSpriteId => 267;
