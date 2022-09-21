5e4606a7b12285b478f13556126ec8be1e2a3fc5
Dfulop98 <dobo.fulop@icloud.com>
2022-09-21 15:19:33 +0200

    Feat: Door element, able to open close

---
+++ Assets/Source/Actors/Static/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Serialization;
+
+namespace DungeonCrawl.Actors.Static
+{
+    public class Door: Actor
+    {
+        private string DoorId = "kenney_transparent_146";
+        //true if closed
+        public bool IsClosed = true;
+
+        public override bool Detectable => IsClosed;
+        public override string DefaultSpriteId => DoorId;
+        public override string DefaultName => "Door";
+
+        public void DoorOpen()
+        {
+            DoorId = "kenney_transparent_147";
+            IsClosed = false;
+        }
+        
+    }
+}
