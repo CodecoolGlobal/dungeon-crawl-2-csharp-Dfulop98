81e50278f25206aa98d00eda01c840b82e51a3b6
Dfulop98 <dobo.fulop@icloud.com>
2022-10-04 20:16:02 +0200

    Add: Create sprite collection class

--- Assets/Source/Actors/SpritesCollection/Sprites.cs
+++ Assets/Source/Actors/SpritesCollection/Sprites.cs
@@ -5,36 +5,94 @@ namespace Assets.Source.Actors.SpritesCollection
 {
     static class Sprites
     {
-        public static List<string> PlayerIdleSprites
+
+        // Warrior Player Sprite collection
+        public static List<string> Warrior
             = new List<string>()
             {
-                "PlayerIdle1",
-                "PlayerIdle2",
-                "PlayerIdle3",
-                "PlayerIdle4",
+                "Warrior01",
+                "Warrior02",
+                "Warrior03",
+                "Warrior04",
             };
+
+        public static List<string> WarriorWithSpear
+            = new List<string>()
+            {
+                "WarriorSpear01",
+                "WarriorSpear02",
+                "WarriorSpear03",
+                "WarriorSpear04",
+            };
+
+        public static List<string> WarriorWithSword
+            = new List<string>()
+            {
+                "WarriorSword01",
+                "WarriorSword02",
+                "WarriorSword03",
+                "WarriorSword04",
+            };
+
+
+        public static List<string> WarriorArmor
+            = new List<string>()
+            {
+                "WarriorArmor01",
+                "WarriorArmor02",
+                "WarriorArmor03",
+                "WarriorArmor04",
+
+            };
+
+        public static List<string> WarriorArmorSword
+            = new List<string>()
+            {
+                "WarriorArmorSword01",
+                "WarriorArmorSword02",
+                "WarriorArmorSword03",
+                "WarriorArmorSword04",
+
+            };
+
+        public static List<string> WarriorArmorSpear
+            = new List<string>()
+            {
+                "WarriorArmorSpear01",
+                "WarriorArmorSpear02",
+                "WarriorArmorSpear03",
+                "WarriorArmorSpear04",
+
+            };
+
+
+
+        // Wizard Sprites
+
+        public static List<string> Wizard
+            = new List<string>()
+            {
+                "Wizard01",
+                "Wizard02",
+                "Wizard03",
+                "Wizard04",
+
+            };
+
+        public static List<string> PlayerWizardWithWand
+            = new List<string>()
+            {
+                "PlayerWizardWand1",
+                "PlayerWizardWand2",
+                "PlayerWizardWand3",
+                "PlayerWizardWand4",
+
+            };
+
+
+        // Item Sprites
         
-        public static List<string> PlayerWSPearSprites
-            = new List<string>()
-            {
-                "PlayerWSpear1",
-                "PlayerWSpear2",
-                "PlayerWSpear3",
-                "PlayerWSpear4",
-
-            };
-
-        public static List<string> PlayerWizardSprites
-            = new List<string>()
-            {
-                "PlayerWizard1",
-                "PlayerWizard2",
-                "PlayerWizard3",
-                "PlayerWizard4",
-
-            };
-
-        public static Dictionary<string, string> ItemSprites
+        public static Dictionary<string, string> Item
             = new Dictionary<string, string>()
             {
                 {"HealthPotion", "kenney_transparent_896"},
