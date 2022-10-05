9c17e8c123f9ae599114a5ba9cf75921c30836ec
Gergely Kamaras <gergely.kamaras@gmail.com>
2022-10-05 11:22:42 +0200

    Add: text field to choice scene

--- Assets/Source/Core/MainMenu.cs
+++ Assets/Source/Core/MainMenu.cs
@@ -5,6 +5,12 @@ namespace Assets.Source.Core
 {
     internal class MainMenu : MonoBehaviour
     {
+        public string PlayerName = "Röszkei Rambó";
+        public void GoToChoiceScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
         public void NewGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
