bac0f54e7677e86b2c1a72c4c550e7f5f6e6069c
Dfulop98 <dobo.fulop@icloud.com>
2022-10-05 15:53:04 +0200

    Feat: wand item class

---
+++ Assets/Source/Actors/Items/Stick.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Source.Actors.SpritesCollection;
+using Assets.Source.Core;
+using DungeonCrawl.Actors.Characters;
+using DungeonCrawl.Core;
+
+
+namespace Assets.Source.Actors.Items
+{
+    internal class Stick : Item
+    {
+        public override string DefaultName => "Pálca";
+        public override string DefaultSpriteId => Sprites.Item[DefaultName];
+
+        public override void Pickup(Player player)
+        {
+            // Apply change
+            player.Damage += 10;
+            player.Inventory.Add(this);
+            UpdateSprite(player);
+            ActorManager.Singleton.DestroyActor(this);
+            EventLog.AddEvent($"{player.Name} picks up {DefaultName}");
+
+        }
+        private void UpdateSprite(Player player)
+        {
+            if (player.UsedSpriteCollection == Sprites.Wizard
+                || player.UsedSpriteCollection == Sprites.WizardWithWand)
+                player.UsedSpriteCollection = Sprites.WizardWithStick;
+            else if (player.UsedSpriteCollection == Sprites.WizardBlanket
+                || player.UsedSpriteCollection == Sprites.WizardWandBlanket)
+                player.UsedSpriteCollection = Sprites.WizardStickBlanket;
+        }
+    }
+}
---
+++ Assets/Source/Actors/Items/Wand.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Source.Actors.SpritesCollection;
+using Assets.Source.Core;
+using DungeonCrawl.Actors.Characters;
+using DungeonCrawl.Core;
+
+
+namespace Assets.Source.Actors.Items
+{
+    internal class Wand : Item
+    {
+        public override string DefaultName => "Brutál Pálca";
+        public override string DefaultSpriteId => Sprites.Item[DefaultName];
+
+        public override void Pickup(Player player)
+        {
+            // Apply change
+            player.Damage += 1000;
+            player.Inventory.Add(this);
+            UpdateSprite(player);
+            ActorManager.Singleton.DestroyActor(this);
+            EventLog.AddEvent($"{player.Name} picks up {DefaultName}");
+
+        }
+        private void UpdateSprite(Player player)
+        {
+            if (player.UsedSpriteCollection == Sprites.Wizard
+                || player.UsedSpriteCollection == Sprites.WizardWithStick)
+                player.UsedSpriteCollection = Sprites.WizardWithWand;
+            else if (player.UsedSpriteCollection == Sprites.WizardBlanket
+                || player.UsedSpriteCollection == Sprites.WizardStickBlanket)
+                player.UsedSpriteCollection = Sprites.WizardWandBlanket;
+        }
+    }
+}
