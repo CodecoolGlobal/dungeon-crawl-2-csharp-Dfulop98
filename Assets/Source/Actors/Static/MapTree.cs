27261292774db616df42e2fad9fbeb63da046340
Dfulop98 <dobo.fulop@icloud.com>
2022-10-06 14:24:41 +0200

    Refactor: new tree sprite

---
+++ Assets/Source/Actors/Static/MapTree.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DungeonCrawl.Actors.Static
+{
+    public class MapTree : Actor
+    {
+      
+        public override char MapIcon => 'T';
+ 
+        public override string DefaultSpriteId => "MapTree";
+        public override string DefaultName => "Tree";
+    }
+}
