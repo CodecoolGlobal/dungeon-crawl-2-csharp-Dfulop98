e23bd5ff18786ac1c0920e4a06df0b202a83397d
Gergely Kamaras <gergely.kamaras@gmail.com>
2022-10-06 17:12:41 +0200

    Balance: rename enemies and adjust properties

--- Assets/Source/Actors/Characters/Enemy/Mushroom.cs
+++ Assets/Source/Actors/Characters/Enemy/Mushroom.cs
@@ -11,7 +11,7 @@ namespace DungeonCrawl.Actors.Characters
         public override int Damage { get; set; } = 10;
         public override int Health { get; set; } = 20;
         public override int ScoreValue { get; set; } = 10;
-        public override string DefaultName => "Angry Mushroom";
+        public override string DefaultName => "Gomba János";
 
         // Sprite Handle
         
